4ed0beb7349dddc74e5cb42df40b3cfcd8b4550a
malharmi <muhannadalharmi@email.arizona.edu>
2021-07-03 09:41:19 +0300

    deleted redundant code

--- ui/script.js
+++ ui/script.js
@@ -1,17 +1,15 @@
-// Initialize params
+const responseBody = document.getElementById("responseBody");
+
+//Initialize params
 url = null;
 requestType = null;
 bodyType = null;
 bodyContent = null;
 
-let submit = document.getElementById("subBtn");
-
-submit.onclick = () => {
+function submission() {
     url = document.getElementById("url").value;
     requestType = document.getElementById("requestType").value;
     bodyType = document.getElementById("bodyType").value;
     bodyContent = document.getElementById("body").value;
     console.log(url, requestType, bodyType, bodyContent);
-};
-
-// handle requests
+}
